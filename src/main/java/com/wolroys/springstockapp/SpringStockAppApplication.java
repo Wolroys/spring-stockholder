d2bd4dce4e0a3a1e6cced99efd93b00b6c3c1682
Wolroys <alekseypaslenov@gmail.com>
2023-12-15 09:15:10 +0300

    added Stoock controller

---
+++ src/main/java/com/wolroys/springstockapp/SpringStockAppApplication.java
@@ -0,0 +1,13 @@
+package com.wolroys.springstockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStockAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wolroys/springstockapp/SpringStockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolroys.springstockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
